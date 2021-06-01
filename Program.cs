212efe697f30309488e1e090aafed5468f6a7ac9
Mohammed Rashed <Mohammed.Albalawi@tuwaiq.edu.sa>
2021-06-01 20:56:24 +0300

    Factory Method Practice

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+
+    public class Creator
+    {
+        public IAvocado getAvocado(int season)
+        {
+
+            if(season < 5)
+            {
+                return new SpanishAvocado();
+            }
+            else if (season < 11 && season > 5)
+            {
+                return new SouthAfricanAvocado();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+
+    /////////////////////////////////////////////
+    public interface IAvocado
+    {
+        public string deliver();
+    }
+
+    public class SouthAfricanAvocado : IAvocado
+    {
+        public string deliver()
+        {
+            return "SOUTH AFRICAN AVOCADO";
+        }
+    }
+
+    public class SpanishAvocado : IAvocado
+    {
+        public string deliver()
+        {
+            return "SPANISH AVOCADO";
+        }
+    }
+
+    /////////////////////////////////////////////
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creator creator = new Creator();
+            Console.WriteLine(creator.getAvocado(7).deliver());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # FactoryMethod
 this repo contains Factory Method design pattern implementation using C#
+
+<hr />
+
+
+
+<br />
+<b> Student:</b>  Mohammed Rashed Albalawi.
+<br />
+<b> Group:</b>  Al-qiddiyah.
+<br />
+<b> Group Supervisor:</b>  Noorah Abdullah.
+
+
+<br />
+<br />
