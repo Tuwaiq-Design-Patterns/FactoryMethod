122c7acf815377cfffd5738b51f7500c5ccf2a03
teefeid <taef_415@hotmail.com>
2021-06-02 00:13:46 +0300

    Factory method concept

---
+++ Factory/ConsoleApp16/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace FactoryConcept
+{
+
+    public interface IStore
+    {
+        string Ship();
+    }
+    public class UsSupplier : IStore
+    {
+        public string Ship() // the Supplier ship to store
+        {
+            return " US Supplier";
+        }
+    }
+    public class SouthAfricaSupplier : IStore
+    {
+        public string Ship()
+        {
+            return "South Africa Supplier ";
+        }
+    }
+
+    public class Creator 
+    {
+        public IStore FactoryMethod(int month)
+        {
+            if (month <= 6)
+            {
+                return new UsSupplier();
+            }
+            else if ((7 <= month) && (month <= 12))
+            {
+                return new SouthAfricaSupplier();
+            }
+            else if (month > 12)
+            {
+                Console.WriteLine("There is no season"); //the default value
+            }
+
+            return null;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)  
+        {
+            Creator c = new Creator();
+            IStore store;
+
+            for (int i = 0; i <=13; i++)//13 Because of testing the default value
+            {
+                store = c.FactoryMethod(i);
+                Console.WriteLine(store.Ship());
+
+            }
+           
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# FactoryMethod
-this repo contains Factory Method design pattern implementation using C#
+# Factory Method in simple word:
+Is creational design pattern, and its idea like a factory provides all of the client's needs.
+It depends on what the client needs, whether for the product or for the service.
+
+# Factory example :
+* Product
+* Spa services
+* Fruit season
+
+# About Factory:
+* It communicates with the interface through the class applied to the interface.
+
+* Factory helps expand, because it separates the construction from the rest of the code.
+
+* Factory Method helps reusing existing objects.
