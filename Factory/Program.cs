3a37f54b796781b54c2341b267bc870a8ab92429
Abdulrahman_Aljafar <abdulrahmana.jafar@gmail.com>
2021-06-02 17:32:37 +0300

    Factory

---
+++ Factory/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Factory
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creator c = new Creator();
+            Iproduct product;
+
+            product = c.factoryMethod();
+            Console.WriteLine(product.Ship());
+            
+        }
+    }
+
+    public interface Iproduct
+    {
+        string Ship();
+    }
+    public class ProductSpain : Iproduct
+    {
+        public string Ship()
+        {
+            return "Spain";
+        }
+    }
+
+    public class ProductSouthAfrica : Iproduct
+    {
+        public string Ship()
+        {
+            return "South Africa";
+        }
+    }
+
+
+    public class ProductNotFound : Iproduct
+    {
+        public string Ship()
+        {
+            return "Not Found";
+        }
+    }
+
+    public class Creator
+    {
+        public Iproduct factoryMethod()
+        {
+            int monthOfPurch = DateTime.Now.Month ;
+            if (monthOfPurch <= 4 && monthOfPurch >= 1)
+            {
+                return new ProductSpain();
+            }
+            else if (monthOfPurch <= 11 && monthOfPurch >= 5)
+            {
+                return new ProductSouthAfrica();
+            }
+            else
+            {
+                return new ProductNotFound();
+            }
+        }
+    }
+
+}
