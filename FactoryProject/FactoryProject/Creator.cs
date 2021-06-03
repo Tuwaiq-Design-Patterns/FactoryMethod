884c4ed1790102042b956b25715f5f664663339f
Ahmed <dev@test.com>
2021-06-03 16:13:41 +0300

    Done

---
+++ FactoryProject/FactoryProject/Creator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace FactoryProject
+{
+    public class Creator
+    {
+        
+
+        public String Factory(int month)
+        {
+            if (month>0 && month <= 4)
+                return new AvogadroA().Name;
+            else if(month>=5 && month<=11)
+                return new AvogadroB().Name;
+            return "not avalibe right now";
+
+        }
+
+    }
+
+   public interface IAvogadro
+    {
+        public String getName();
+    }
+
+   public class AvogadroA : IAvogadro
+    {
+        
+        public String Name = "Avogadro From spain";
+
+        public String getName()
+        {
+            return this.Name;
+        }
+    }
+
+
+    public class AvogadroB : IAvogadro
+    {
+
+        public String Name= "Avogadro From S.A.";
+
+        public AvogadroB()
+        {
+            this.getName();
+        }
+
+        public String getName()
+        {
+            return this.Name;
+        }
+    }
+}
---
+++ FactoryProject/FactoryProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactoryProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((new Creator().Factory(1)));
+        }
+    }
+}
