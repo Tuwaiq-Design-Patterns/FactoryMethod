af3bf6546306728aef0960721add474e304d2510
aalhaif <Abdullah.Alhaif@Tuwaiq.edu.sa>
2021-06-03 13:06:43 +0300

    FactoryMethod HW

---
+++ Factory/Class0601-DesignPattern/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace FactoryMethodTest
+{
+    public interface ITransportation
+    {
+        string Make();
+    }
+    public class Car : ITransportation
+    {
+        public String Make()
+        {
+            return "Car";
+        }
+    }
+    public class Motorcycle : ITransportation
+    {
+        public string Make()
+        {
+            return "Bike";
+        }
+    }
+    public class Bus : ITransportation
+    {
+        public string Make()
+        {
+            return "Bus";
+        }
+    }
+    public class Creator
+    {
+        public ITransportation Manufactory(int month)
+        {
+            if (month is > 0 and < 12)
+            {
+                return month < 5 ? new Car() : new Motorcycle();
+            }
+            return new Bus();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITransportation avocado;
+            Creator creator = new Creator();
+            avocado = creator.Manufactory(2);
+            Console.WriteLine(avocado.Make());
+            avocado = creator.Manufactory(6);
+            Console.WriteLine(avocado.Make());
+            avocado = creator.Manufactory(12);
+            Console.WriteLine(avocado.Make());
+        }
+    }
+}
