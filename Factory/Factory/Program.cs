fbb512837b51ab820de2d529e50d4411b6171807
SamirahAlhusayni <samirah.alhusayni@tuwaiq.edu.sa>
2021-06-03 16:37:32 +0300

    FactoryMethod

---
+++ Factory/Factory/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace Factory
+{
+    public interface IAvocado
+    {
+        string Delever();
+    }
+    public class SpainAvocado : IAvocado
+    {
+        public string Delever()
+        {
+            return "California";
+        }
+    }
+    public class SouthAfricaAvocado : IAvocado
+    {
+        public string Delever()
+        {
+            return "Mexico";
+        }
+    }
+    public class NoAvocado : IAvocado
+    {
+        public string Delever()
+        {
+            return "Sorry, There is no Avocado";
+        }
+    }
+    public class Creator
+    {
+        public IAvocado FactoryMethod(int month)
+        {
+            if (month is > 0 and < 12)
+            {
+                return month < 5 ? new SpainAvocado() : new SouthAfricaAvocado();
+            }
+            return new NoAvocado();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAvocado avocado;
+            Creator creator = new Creator();
+            avocado = creator.FactoryMethod(2);
+            Console.WriteLine(avocado.Delever());
+            avocado = creator.FactoryMethod(6);
+            Console.WriteLine(avocado.Delever());
+            avocado = creator.FactoryMethod(12);
+            Console.WriteLine(avocado.Delever());
+        }
+    }
+}
